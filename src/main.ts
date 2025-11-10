436b19bfab4ef1e7ad3680bfa192ab949ef947b0
Linda <lindakjellberg7@gmail.com>
2025-11-10 14:07:24 +0100

    Add environmental variable test

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,6 @@
 import './style.css'
 
+/* =========================================== API =========================================== */
+// Use the fetch function to get a list of objects from an API on only 5 rows:
+// Add API to a .env file to store it saftely
+
