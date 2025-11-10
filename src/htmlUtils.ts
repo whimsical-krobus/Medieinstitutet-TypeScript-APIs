c83b8493d2b4eabdae11354d4dc3ea8c0927db8b
Linda <lindakjellberg7@gmail.com>
2025-11-10 15:58:09 +0100

    Add comment

--- src/htmlUtils.ts
+++ src/htmlUtils.ts
@@ -1,2 +1,2 @@
-/* Display: common_name, scientific_name, family, genus, default_image */
+/* Loop through and display: common_name, scientific_name, family, genus, default_image for each Plant */
 
