0474ad23621da5a573a26cbe7a320d2bcbddce99
Linda <lindakjellberg7@gmail.com>
2025-11-10 14:33:19 +0100

    Add fetch using api variable

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
-import './style.css'
+import "./style.css";
 
 /* =========================================== API =========================================== */
 // Use the fetch function to get a list of objects from an API on only 5 rows:
 // Add API to a .env file to store it saftely
 
+fetch(import.meta.env.VITE_API_URL)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  });
